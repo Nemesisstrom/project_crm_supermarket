7f5833d96e26bd3f95a149c291d9b492907afc86
Muhammad Fackhri Mahesa Putra <muhammadfackhrimahesaputra@gmail.com>
2026-04-29 16:39:46 +0700

    first commit

---
+++ database/migrations/2026_04_28_041257_create_detail_penjualan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_penjualan', function (Blueprint $table) {
+            $table-> id();
+            $table->foreignId('penjualan_id')->constrained()->onDelete('cascade')
+            $table->foreignId('produk_id')->constrained()->onDelete('cascade')
+            $table->integer('qty');    
+            $table->decimal('harga');
+            $table->decimal('subtotal');     
+        }) 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
